bad7afbab558f46ac2a1c0be959c6c23119ca4c9
liqiang <1725055744@qq.com>
2020-04-21 23:12:39 +0800

    spring's reactiveStringRedisTemplate

--- base/src/main/java/top/itlq/redis/base/dao/RedisDao.java
+++ base/src/main/java/top/itlq/redis/base/dao/RedisDao.java
@@ -2,25 +2,60 @@ package top.itlq.redis.base.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.ReactiveValueOperations;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class RedisDao {
 
-    private final ReactiveRedisTemplate<Object, Object> reactiveRedisTemplate;
+    private final ReactiveStringRedisTemplate reactiveStringRedisTemplate;
+
+    private final ReactiveValueOperations<byte[], byte[]> opsForValue;
 
     @Autowired
-    public RedisDao(ReactiveRedisTemplate<Object, Object> reactiveRedisTemplate){
-        this.reactiveRedisTemplate = reactiveRedisTemplate;
+    public RedisDao(ReactiveStringRedisTemplate reactiveStringRedisTemplate){
+        this.reactiveStringRedisTemplate = reactiveStringRedisTemplate;
+        opsForValue = reactiveStringRedisTemplate.opsForValue(RedisSerializationContext.byteArray());
     }
 
     public Mono<Boolean> set(String key, String value){
-        return reactiveRedisTemplate.opsForValue().set(key.getBytes(), value.getBytes());
+        return set(key, value.getBytes(StandardCharsets.UTF_8));
     }
 
-    public String get(String key){
-        return reactiveRedisTemplate.opsForValue().get(key.getBytes())
-                .map(v->new String((byte[])v)).block();
+    public Mono<Boolean> set(String key, byte[] bytes){
+        return opsForValue.set(key.getBytes(StandardCharsets.UTF_8), bytes);
+    }
+
+    public Mono<String> get(String key){
+        return opsForValue.get(key.getBytes(StandardCharsets.UTF_8))
+                .map(v->new String(v, StandardCharsets.UTF_8));
+    }
+
+    public Mono<Boolean> del(String key){
+        return opsForValue.delete(key.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public Flux<String> scan(String pattern){
+        ScanOptions scanOptions = new ScanOptions.ScanOptionsBuilder()
+                .count(0)
+                .match(pattern)
+                .build();
+        return reactiveStringRedisTemplate.scan(scanOptions);
+    }
+
+    /**
+     * not in production environment
+     * @param pattern
+     * @return
+     */
+    public Flux<String> keys(String pattern){
+        return reactiveStringRedisTemplate.keys(pattern);
     }
 }
--- base/src/test/java/top/itlq/redis/base/dao/RedisDaoTest.java
+++ base/src/test/java/top/itlq/redis/base/dao/RedisDaoTest.java
@@ -1,10 +1,15 @@
 package top.itlq.redis.base.dao;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 import top.itlq.redis.base.dao.RedisDao;
+
+import java.util.Arrays;
 
 @SpringBootTest
 class RedisDaoTest {
@@ -12,9 +17,58 @@ class RedisDaoTest {
     @Autowired
     RedisDao redisDao;
 
+    @BeforeEach
+    void beforeEach(){
+        String[] keys1 = {"ab", "ac", "ad"};
+        String[] keys2 = {"ba", "bc"};
+        byte[] values = {0};
+        Assert.assertEquals(
+                Flux.fromArray(keys1).concatWith(Flux.fromArray(keys2))
+                        .flatMap(key -> redisDao.set(key, values))
+                        .reduce(Boolean::logicalAnd).block(),
+                true
+        );
+    }
+
     @Test
     void testSet(){
-        redisDao.set("a", "ccc");
-        Assert.assertEquals(redisDao.get("a"), "ccc");
+        String value = "测试";
+        Assert.assertEquals(true, redisDao.set("a", value).block());
+        Assert.assertEquals(redisDao.get("a").block(), value);
+        Assert.assertEquals(true, redisDao.del("a").block());
     }
+
+    @Test
+    void testKeys(){
+        String[] keys1 = {"ab", "ac", "ad"};
+        Assert.assertTrue(
+                redisDao.keys("a*")
+                        .collectList().blockOptional()
+                        .orElseThrow()
+                        .containsAll(Arrays.asList(keys1))
+        );
+    }
+
+    @Test
+    void testScan(){
+//        String[] keys1 = {"ab", "ac", "ad"};
+//        String[] keys2 = {"ba", "bc"};
+        System.out.println(
+                redisDao.scan("a*").collectList().block()
+        );
+    }
+
+    @AfterEach
+    void afterEach(){
+        String[] keys1 = {"ab", "ac", "ad"};
+        String[] keys2 = {"ba", "bc"};
+        byte[] values = {0};
+        Assert.assertEquals(
+                Flux.fromArray(keys1).concatWith(Flux.fromArray(keys2))
+                        .flatMap(redisDao::del)
+                        .reduce(Boolean::logicalAnd).block(),
+                true
+        );
+    }
+
 }
